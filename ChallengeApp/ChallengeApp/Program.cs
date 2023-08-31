89ec992eff555ea6e61971c4192f226bee828af8
MariuszLeonGebel <mariusz.gebel@wsth.plm>
2023-08-31 22:17:13 +0200

    Zadanie domowe - Dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World! Pierwsza moja aplikacja w C# zintegrowana z Gitem");
+﻿string name = "Karolina";
+char sex = 'K';
+int age = 32;
+
+if(sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat.");
+}
+else if(name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33.");
+} 
+else if(sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna.");
+}
+else
+{
+    Console.WriteLine("Nie jesteś ani Kobietą poniżej 30 lat ani Ewą, lat 33 ani Niepełnoletnim Mężczyzną!");
+}
+
+
+
