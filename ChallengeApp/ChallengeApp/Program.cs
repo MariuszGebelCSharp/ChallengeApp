294797de6837914d1915d009aca2b28007eb3e4e
MariuszLeonGebel <mariusz.gebel@wsth.plm>
2023-09-01 15:17:36 +0200

    For-ForEach-Array-List

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,23 +1,38 @@
-﻿string name = "Karolina";
-char sex = 'K';
-int age = 32;
+﻿int[] grades = new int[365];
+string[] daysOfWeek = new string[7];
+daysOfWeek[0] = "poniedziałek";
+daysOfWeek[1] = "wtorek";
+daysOfWeek[2] = "środa";
+daysOfWeek[3] = "czwartek";
+daysOfWeek[4] = "piątek";
+daysOfWeek[5] = "sobota";
+daysOfWeek[6] = "niedziela";
 
-if(sex == 'K' && age < 30)
+Console.WriteLine(daysOfWeek[2]);
+
+string[] daysOfWeek1 = {"poniedziałek", "wtorek", "środa" };
+
+Console.WriteLine(daysOfWeek[0]);
+
+for(int i=0; i<daysOfWeek.Length; i=i+2)
 {
-    Console.WriteLine("Kobieta poniżej 30 lat.");
+    Console.WriteLine(daysOfWeek[i]);
 }
-else if(name == "Ewa" && age == 33)
+
+List<string> dayOfWeek =  new List<string>();
+
+dayOfWeek.Add("poniedziałek");
+dayOfWeek.Add("wtorek");
+dayOfWeek.Add("środa");
+dayOfWeek.Add("czwartek");
+dayOfWeek.Add("piątek");
+dayOfWeek.Add("sobota");
+dayOfWeek.Add("niedziela");
+
+for (int i = 0; i < dayOfWeek.Count; i++)
 {
-    Console.WriteLine("Ewa, lat 33.");
-} 
-else if(sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna.");
+    Console.WriteLine("Dzień tygodnia nr "+ (i+1) + " to: " + dayOfWeek[i]);
 }
-else
-{
-    Console.WriteLine("Nie jesteś ani Kobietą poniżej 30 lat ani Ewą, lat 33 ani Niepełnoletnim Mężczyzną!");
-}
 
-
-
+foreach(var day in dayOfWeek)
+{ Console.WriteLine(day); } 
