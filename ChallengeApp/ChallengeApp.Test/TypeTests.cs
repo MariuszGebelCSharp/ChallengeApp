0872d1059354c9f974243cd5d080217d16f1d315
MariuszLeonGebel <mariusz.gebel@wsth.plm>
2023-09-04 23:06:24 +0200

    Zadanie domowe - Dzień 8 (Value and Reference Data Types

---
+++ ChallengeApp/ChallengeApp.Test/TypeTests.cs
@@ -0,0 +1,64 @@
+﻿namespace ChallengeApp.Test
+{
+    public class TypeTests
+    {
+        [Test]
+        public void IntType_ValueType()
+        {
+            // arrange
+            int number1 = 3;
+            int number2 = 2;    
+     
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void FloatType_ValueType()
+        {
+            // arrange
+            float number1 = 33.89F;
+            float number2 = 3.12F;
+
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void DoubleType_ValueType()
+        {
+            // arrange
+            double number1 = 3.8;
+            double number2 = 3.8;
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        private User GetUser(string name)
+        {
+            return new User(name);
+        }
+
+        [Test]
+        public void ComparingTwoStrings()
+        {
+            // arrange
+            var user1 = GetUser("Adam");
+            var user2 = GetUser("Adam");
+            // act
+
+            // assert
+            Assert.AreEqual(user1.Login, user2.Login);
+        }
+ 
+        [Test]
+        public void GetUserShouldReturnDifferentObjects()
+        {
+            // arrange
+            var user1 = GetUser("Adam");
+            var user2 = GetUser("Adam");
+            // act
+
+            // assert
+            Assert.AreNotEqual(user1, user2);
+        }
+    }
+}
